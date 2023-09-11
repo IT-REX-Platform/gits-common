a0ce6ff79cd12315426b90970ae05ef2c7534f4b
PaulBredl <paulbredl@gmx.de>
2023-09-11 18:28:42 +0200

    revert LoggedInUser

--- src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
+++ src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
@@ -9,12 +9,19 @@ import java.util.UUID;
  * This class represents user data for a logged-in user as provided by keycloak.
  */
 @Getter
-public record LoggedInUser(
-        @JsonProperty("id") UUID id,
-        @JsonProperty("userName") String userName,
-        @JsonProperty("firstName") String firstName,
-        @JsonProperty("lastName") String lastName
-) {
+public class LoggedInUser {
+    private final UUID id;
+    private final String userName;
+    private final String firstName;
+    private final String lastName;
 
+    public LoggedInUser(@JsonProperty("id") UUID id,
+                        @JsonProperty("userName") String userName,
+                        @JsonProperty("firstName") String firstName,
+                        @JsonProperty("lastName") String lastName) {
+        this.id = id;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
-
