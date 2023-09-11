9c4d811c4a122870bced0b7f3e1e588860259f1a
PaulBredl <paulbredl@gmx.de>
2023-09-11 18:23:27 +0200

    remove resource markdown

--- src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
+++ src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
@@ -1,6 +1,5 @@
 package de.unistuttgart.iste.gits.common.user_handling;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
@@ -10,19 +9,12 @@ import java.util.UUID;
  * This class represents user data for a logged-in user as provided by keycloak.
  */
 @Getter
-public class LoggedInUser {
-    private final UUID id;
-    private final String userName;
-    private final String firstName;
-    private final String lastName;
+public record LoggedInUser(
+        @JsonProperty("id") UUID id,
+        @JsonProperty("userName") String userName,
+        @JsonProperty("firstName") String firstName,
+        @JsonProperty("lastName") String lastName
+) {
 
-    public LoggedInUser(@JsonProperty("id") UUID id,
-                        @JsonProperty("userName") String userName,
-                        @JsonProperty("firstName") String firstName,
-                        @JsonProperty("lastName") String lastName) {
-        this.id = id;
-        this.userName = userName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
 }
+
