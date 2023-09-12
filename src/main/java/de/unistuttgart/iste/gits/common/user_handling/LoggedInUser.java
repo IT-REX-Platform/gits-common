ba951d6bfdaedae70eb1221546002914bf04ebeb
myluki2000 <lukas636@live.com>
2023-09-12 02:44:35 +0200

    LoggedInUser: Add course membership data

--- src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
+++ src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
@@ -3,6 +3,7 @@ package de.unistuttgart.iste.gits.common.user_handling;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -14,14 +15,34 @@ public class LoggedInUser {
     private final String userName;
     private final String firstName;
     private final String lastName;
+    private final List<CourseMembership> courseMemberships;
 
     public LoggedInUser(@JsonProperty("id") UUID id,
                         @JsonProperty("userName") String userName,
                         @JsonProperty("firstName") String firstName,
-                        @JsonProperty("lastName") String lastName) {
+                        @JsonProperty("lastName") String lastName,
+                        @JsonProperty("courseMemberships") List<CourseMembership> courseMemberships) {
         this.id = id;
         this.userName = userName;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.courseMemberships = courseMemberships;
+    }
+
+    @Getter
+    public static class CourseMembership {
+        private final UUID courseId;
+        private final UserRoleInCourse role;
+
+        public CourseMembership(@JsonProperty("courseId") UUID courseId, @JsonProperty("role") UserRoleInCourse role) {
+            this.courseId = courseId;
+            this.role = role;
+        }
+    }
+
+    public enum UserRoleInCourse {
+        STUDENT,
+        TUTOR,
+        ADMINISTRATOR
     }
 }
