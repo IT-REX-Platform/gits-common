a245abd185b976e653f878fb99dbfcb8ee9fb8e0
myluki2000 <lukas636@live.com>
2023-09-27 15:43:18 +0200

    Format and add doc comments

--- src/main/java/de/unistuttgart/iste/gits/common/exception/NoAccessToCourseException.java
+++ src/main/java/de/unistuttgart/iste/gits/common/exception/NoAccessToCourseException.java
@@ -2,6 +2,9 @@ package de.unistuttgart.iste.gits.common.exception;
 
 import java.util.UUID;
 
+/**
+ * Exception thrown when a user does not have access to a course.
+ */
 public class NoAccessToCourseException extends RuntimeException {
     public NoAccessToCourseException(UUID courseId) {
         super("User has no access to course with id " + courseId + ".");
